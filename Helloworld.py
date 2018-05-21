ad3645350ff47532bf75a3d7fa622cfba67e084d
anishSachdeva <anish_@outlook.com>
2018-05-21 20:30:38 +0530

    performance difference between numpy and python arrays

--- Helloworld.py
+++ Helloworld.py
@@ -2,3 +2,9 @@ import numpy as np
 
 array = np.array([1, 10, 5, 6])
 print(array)
+
+pyList = range(1000)
+print(pyList.__len__())
+
+print(pyList.__sizeof__())
+print(array.size)
---
+++ numpy vs py.py
@@ -0,0 +1,7 @@
+import numpy as np
+import time
+import sys
+
+SIZE = 10000
+pyList = range(SIZE)
+npList = np.arange(SIZE)
