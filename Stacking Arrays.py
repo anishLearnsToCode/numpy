2d2fb34359a3841570bfb8201d7f6ee9e75bf878
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-22 09:21:17 +0530

    added content to stacking arrays

--- Stacking Arrays.py
+++ Stacking Arrays.py
@@ -1,1 +1,10 @@
-# Stacking Arrays 
+# Stacking Arrays
+import numpy as np
+
+array = np.arange(12).reshape(3,4)
+print(array)
+array = array + 1
+print(array)
+
+checkArray = array > 4
+print(checkArray)
